47810118d1cf9826494f9eed95d66fa179d249bf
Diego <diego.fobs@gmail.com>
2026-03-30 22:03:37 -0300

    Finished the calculator and did other minor adjustments

--- Semana 5/Entrega/Exercício 4.py
+++ Semana 5/Entrega/Exercício 4.py
@@ -1,5 +1,15 @@
 i = 0
 numero = 0
+par = 0
+impar = 0
 
-while != 10
-    numero
+while i < 10:
+    numero = float(input("Insira um número: "))
+    i += 1
+    if numero % 2 == 0:
+        par += 1
+    else:
+        impar += 1
+
+print(f"Você digitou {par} número(s) pares.")
+print(f"Você digitou {impar} número(s) ímpares.")
---
+++ Semana 5/Entrega/Exercício 5.py
@@ -0,0 +1,30 @@
+import time # Importar timer
+i = ()
+
+while not i: # Loop infinito
+    print("Escolha um operação:")
+    print("1: Soma")
+    print("2: Substração")
+    print("3: Multiplicação")
+    print("4: Divisão")
+    print("0: Sair")
+    escolha = int(input("\n"))
+    if escolha == 0: # Sair
+        break
+    if escolha >= 1 and escolha <= 4: # Opções válidas
+        n1 = float(input("\nInforme o primeiro número: "))
+        n2 = float(input("\nInforme o segundo número: "))
+        if escolha == 4 and n2 == 0: #Se estiver dividindo por 0, tentar novamente
+            while n2 == 0:
+                n2 = float(input("\nInforme um segundo valor diferente de zero: "))
+        if escolha == 1:
+            print(f"\nO resultado da operação é {n1 + n2:.2f}.\n")
+        elif escolha == 2:
+            print(f"\nO resultado da operação é {n1 - n2:.2f}.\n")
+        elif escolha == 3:
+            print(f"\nO resultado da operação é {n1 * n2:.2f}.\n")
+        else:
+            print(f"\nO resultado da operação é {n1 / n2:.2f}.\n")
+        time.sleep(3) # Esperar 3 segundos
+    else:
+        print("Insira uma opção válida.\n") # Usuário colocou uma opção inválida
